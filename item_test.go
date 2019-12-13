0a26e9e1480c7038077f04cd9fd77488c68c3e9d
pg <paritosh@noisyox.com>
2019-12-14 00:45:04 +0530

    Add post delete test

--- item_test.go
+++ item_test.go
@@ -14,22 +14,45 @@ import (
 // 	"testing"
 // )
 
-// func TestAction_Post(t *testing.T) {
-// 	setup()
-// 	defer teardown()
-// 	mux.HandleFunc("/v1/items/1", func(w http.ResponseWriter, r *http.Request) {
-// 		testMethod(t, r, http.MethodPost)
-// 	})
+func TestAction_Post(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/v1/items/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+	})
 
-// 	i := Item{ID: "computer-0"}
+	i := &Item{ID: "computer-0"}
 
-// 	resp, err := client.Item.Post(ctx, i)
-// 	if err != nil {
-// 		t.Fatalf("unexpected error: %s", err)
-// 	}
+	resp, err := client.Item.Post(ctx, i)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
 
-// 	fmt.Println(resp)
-// }
+	fmt.Println(resp)
+}
+
+func TestAction_Delete(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/v1/items/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+	})
+
+	s := "aMGYUzAwO0XWByf,99a9w6-Wg,KNIN8eaZg,nSaBueaZg,NVU0s6-ZR,of7RU6-WR,QbqiggBZg,qtZdggBWg,_Bbz_6-Wg,IAH8kbH2Z0NfiH7,cQzPa6-Zg,EoYB93-ZR,EwoWa6-ZR,fknjo6-WR,gr9C-eaZR,hF1I9qaWR,LnC893aWg,t33Or3aZR,VotqXq-Wg,TLrHOMRdzfEcqPm,1_3nDeaZg,2tZeW6aWR,4EVfU6aZR,5HS3ue-Zg,7FxaNe-Wg,7VCdo6-Wg,8Pe-l6aZg,8qC4s6-Wg,92zuQ6-WR,9MYFY6-ZR,A4nwx6aWg,D2RAYe-ZR,dEy9re-ZR,flLddiBZg,I0_ZE6aWg,iAhvPe-ZR,iljrCeaZR,Jgzvf6aZg,JtqpNmBZg,MHWMf6-ZR,nov68eaWg,oYTyeeaZR,plGoXeaZg,Q7cBJ6aZR,QaZgdmBWR,QKAQ16aWg,QW6JI6aZg,RSNV26-Wg,RvPIpe-WR,sUm4i6aZR,tULst6aWg,vwkJx6-Wg,wA7SYeaZg,WWRRfeaWR,XUdpS6aWg,xwBpvmfWR,yc2BJe-Wg,YFaVE6-WR,YVHQSeaWR,Z8n1diBWg,Z9jL_eaWg,ZIYpJe-WR,FwEWy3aWg"
+
+	ids := strings.Split(s, ",")
+
+	for _, id := range ids {
+		i := &Item{ID: id}
+		resp, err := client.Item.Delete(ctx, i)
+		if err != nil {
+			fmt.Println(err)
+			// t.Fatalf("unexpected error: %s", err)
+		}
+
+		fmt.Println(resp)
+	}
+}
 
 func TestAction_AddProp(t *testing.T) {
 	setup()
